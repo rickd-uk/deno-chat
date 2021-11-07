84e955bdbd4d6d862e9d7be1cb03a7ecfad2f9f6
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-11-07 18:09:10 +0900

    Third commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-addEventListener('fetch', (event) => {
-  const response = new Response('Hello Wonderful World!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-  event.respondWith(response)
+import { Application } from 'https://deno.land/x/oak/mod/ts'
+
+const app = new Application()
+app.use((ctx) => {
+  ctx.response.body = 'Hello world third time'
 })
+
+addEventListener('fetch', app.fetchEventHandler())
