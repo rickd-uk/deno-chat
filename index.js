5a48bb89ce651020cf233c2eb256ce44bf8edb67
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-11-07 18:15:37 +0900

    create message app

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts'
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+
+const messages = []
+
+const router = new Router()
+router
+  .get('/', (context) => {
+    context.respose.body = 'Chat server'
+  })
+  .get('/messages', (context) => {
+    context.respose.body = messages
+  })
+  .post('/messages', async (context) => {
+    const message = await context.request.body().value
+    messages.push(message)
+  })
 
 const app = new Application()
-app.use((ctx) => {
-  ctx.response.body = 'Hello world third time'
-})
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 addEventListener('fetch', app.fetchEventHandler())
